cb9f58d42cb7d88fcacc1cbf5d8eead0258c54be
John-Oresh <J.Oresotu@gmail.com>
2023-12-22 13:34:54 +0100

    Improved version

--- hat.js
+++ hat.js
@@ -1,107 +1,177 @@
-console.log("Hello world !!!");
+// console.log("Hello world !!!");
 
-const variable = document.getElementById("cart");
-const inputText = document.getElementById("inputText")
-const first = document.getElementById("first")
-const second = document.getElementById("second")
-const third = document.getElementById("third")
-const signup = document.getElementById("gitHub")
+// const variable = document.getElementById("cart");
+// const inputText = document.getElementById("inputText")
+// const first = document.getElementById("first")
+// const second = document.getElementById("second")
+// const third = document.getElementById("third")
+// const signup = document.getElementById("gitHub")
 
-// variable.addEventListener('click', () => {
-//     return alert("Cart is clicked");
+// // variable.addEventListener('click', () => {
+// //     return alert("Cart is clicked");
+// // })
+
+// const myValue = 0;
+
+// first.addEventListener('click', () => {
+//     const firstValue = first.innerHTML
+
+//     inputText.value = Number(inputText.value) + Number(firstValue)
 // })
 
-const myValue = 0;
+// second.addEventListener('click', () => {
+//     const firstValue = second.innerHTML
 
-first.addEventListener('click', () => {
-    const firstValue = first.innerHTML
-
-    inputText.value = Number(inputText.value) + Number(firstValue)
-})
-
-second.addEventListener('click', () => {
-    const firstValue = second.innerHTML
-
-    inputText.value = Number(inputText.value) + Number(firstValue)
-})
-
-third.addEventListener('click', () => {
-    const firstValue = third.innerHTML
-
-    inputText.value = Number(inputText.value) + Number(firstValue)
-})
-
-signup.addEventListener('click', (event) => {
-    const modal = document.getElementById("modal");
-    modal.classList.toggle("mystyle");
-})
-
-document.getElementById("plusM").addEventListener('click', () => {
-    const firstValue = document.getElementById("plusM").innerHTML
-    inputMango.value = parseInt(inputMango.value) + 1 + "M";
-})
-
-document.getElementById("minusM").addEventListener('click', () => {
-    const firstValue = document.getElementById("minusM").innerHTML
-    inputMango.value = parseInt(inputMango.value) - 1 + "M";
-})
-
-document.getElementById("plusA").addEventListener('click', () => {
-    const firstValue = document.getElementById("plusA").innerHTML;
-    inputApple.value = parseInt(inputApple.value) + 1 + "A";
-});
-
-document.getElementById("minusA").addEventListener('click', () => {
-    const firstValue = document.getElementById("minusA").innerHTML;
-    inputApple.value = parseInt(inputApple.value) - 1 + "A";
-});
-
-document.getElementById("plusP").addEventListener('click', () => {
-    const firstValue = document.getElementById("plusP").innerHTML;
-    inputPlum.value = parseInt(inputPlum.value) + 1 + "P";
-});
-
-document.getElementById("minusP").addEventListener('click', () => {
-    const firstValue = document.getElementById("minusP").innerHTML;
-    inputPlum.value = parseInt(inputPlum.value) - 1 + "P";
-});
-
-document.getElementById("plusO").addEventListener('click', () => {
-    const firstValue = document.getElementById("plusO").innerHTML;
-    inputOrange.value = parseInt(inputOrange.value) + 1 + "O";
-});
-
-document.getElementById("minusO").addEventListener('click', () => {
-    const firstValue = document.getElementById("minusO").innerHTML;
-    inputOrange.value = parseInt(inputOrange.value) - 1 + "O";
-});
-
-document.getElementById("plusG").addEventListener('click', () => {
-    const firstValue = document.getElementById("plusG").innerHTML;
-    inputGrape.value = parseInt(inputGrape.value) + 1 + "G";
-});
-
-document.getElementById("minusG").addEventListener('click', () => {
-    const firstValue = document.getElementById("minusG").innerHTML;
-    inputGrape.value = parseInt(inputGrape.value) - 1 + "G";
-});
-
-
-
-// variable.addEventListener('keypress', (event) => {
-//     if (event.key === "Enter") {
-//         // Cancel the default action, if needed
-//         event.preventDefault();
-//         variable.innerHTML = "Loading ..."
-
-//         const firstValue = first.innerHTML
-
-//         inputText.value = Number(inputText.value) + Number(firstValue)
-
-//         // Trigger the button element with a click
-//         // document.getElementById("myBtn").click();
-//         // return alert("Cart is clicked");
-//       } 
+//     inputText.value = Number(inputText.value) + Number(firstValue)
 // })
 
+// third.addEventListener('click', () => {
+//     const firstValue = third.innerHTML
 
+//     inputText.value = Number(inputText.value) + Number(firstValue)
+// })
+
+// signup.addEventListener('click', (event) => {
+//     const modal = document.getElementById("modal");
+//     modal.classList.toggle("mystyle");
+// })
+
+// document.getElementById("plusM").addEventListener('click', () => {
+//     const firstValue = document.getElementById("plusM").innerHTML
+//     inputMango.value = parseInt(inputMango.value) + 1 + "M";
+// })
+
+// document.getElementById("minusM").addEventListener('click', () => {
+//     const firstValue = document.getElementById("minusM").innerHTML
+//     inputMango.value = parseInt(inputMango.value) - 1 + "M";
+// })
+
+// document.getElementById("plusA").addEventListener('click', () => {
+//     const firstValue = document.getElementById("plusA").innerHTML;
+//     inputApple.value = parseInt(inputApple.value) + 1 + "A";
+// });
+
+// document.getElementById("minusA").addEventListener('click', () => {
+//     const firstValue = document.getElementById("minusA").innerHTML;
+//     inputApple.value = parseInt(inputApple.value) - 1 + "A";
+// });
+
+// document.getElementById("plusP").addEventListener('click', () => {
+//     const firstValue = document.getElementById("plusP").innerHTML;
+//     inputPlum.value = parseInt(inputPlum.value) + 1 + "P";
+// });
+
+// document.getElementById("minusP").addEventListener('click', () => {
+//     const firstValue = document.getElementById("minusP").innerHTML;
+//     inputPlum.value = parseInt(inputPlum.value) - 1 + "P";
+// });
+
+// document.getElementById("plusO").addEventListener('click', () => {
+//     const firstValue = document.getElementById("plusO").innerHTML;
+//     inputOrange.value = parseInt(inputOrange.value) + 1 + "O";
+// });
+
+// document.getElementById("minusO").addEventListener('click', () => {
+//     const firstValue = document.getElementById("minusO").innerHTML;
+//     inputOrange.value = parseInt(inputOrange.value) - 1 + "O";
+// });
+
+// document.getElementById("plusG").addEventListener('click', () => {
+//     const firstValue = document.getElementById("plusG").innerHTML;
+//     inputGrape.value = parseInt(inputGrape.value) + 1 + "G";
+// });
+
+// document.getElementById("minusG").addEventListener('click', () => {
+//     const firstValue = document.getElementById("minusG").innerHTML;
+//     inputGrape.value = parseInt(inputGrape.value) - 1 + "G";
+// });
+
+
+
+// // variable.addEventListener('keypress', (event) => {
+// //     if (event.key === "Enter") {
+// //         // Cancel the default action, if needed
+// //         event.preventDefault();
+// //         variable.innerHTML = "Loading ..."
+
+// //         const firstValue = first.innerHTML
+
+// //         inputText.value = Number(inputText.value) + Number(firstValue)
+
+// //         // Trigger the button element with a click
+// //         // document.getElementById("myBtn").click();
+// //         // return alert("Cart is clicked");
+// //       } 
+// // })
+
+
+const second = document.getElementById("fruit");
+const list = [];
+const mangoP = document.getElementById("plusM");
+const mangoM = document.getElementById("minusM");
+const appleP = document.getElementById("plusA");
+const appleM = document.getElementById("minusA");
+const plumP = document.getElementById("plusP");
+const plumM = document.getElementById("minusP");
+const orangeP = document.getElementById("plusO");
+const orangeM = document.getElementById("minusO");
+const grapeP = document.getElementById("plusG");
+const grapeM = document.getElementById("minusG");
+
+mangoP.addEventListener('click', () => {
+    list.push("Mango");
+    document.getElementById("fruit").value = list;
+    // list.map((item,index) => {
+    //     second.value = item;
+    //     });
+});
+
+
+mangoM.addEventListener('click', () => {
+    list.pop();
+    document.getElementById("fruit").value = list;
+    // list.map((item,index) => {
+    //     console.log(item)
+    //     second.value = item;
+    //     });
+});
+
+appleP.addEventListener('click', () => {
+    list.push("Apple");
+    document.getElementById("fruit").value = list;
+});
+
+appleM.addEventListener('click', () => {
+    list.pop();
+    document.getElementById("fruit").value = list;
+});
+
+plumP.addEventListener('click', () => {
+    list.push("Plum");
+    document.getElementById("fruit").value = list;
+});
+
+plumM.addEventListener('click', () => {
+    list.pop();
+    document.getElementById("fruit").value = list;
+});
+
+orangeP.addEventListener('click', () => {
+    list.push("Orange");
+    document.getElementById("fruit").value = list;
+});
+
+orangeM.addEventListener('click', () => {
+    list.pop();
+    document.getElementById("fruit").value = list;
+});
+
+grapeP.addEventListener('click', () => {
+    list.push("Grape");
+    document.getElementById("fruit").value = list;
+});
+
+grapeM.addEventListener('click', () => {
+    list.pop();
+    document.getElementById("fruit").value = list;
+});
